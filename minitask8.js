1b3e9ba6d920b717386b5c7ce719bac1f60d1ff4
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-26 16:44:06 +0700

    Add files via upload

---
+++ minitask8.js
@@ -0,0 +1,34 @@
+const movie = {
+    id: 1,
+    title: "Avengers",
+    Image: "Alaska",
+    genre: ["Action", "Comedy"],
+    cast: [{name: "Tom Cruise"}, {role: "Hulk"}],
+    getNameJudul: function() {
+        return this.title;
+    },
+    getNameGambar: function() {
+        return this.Image;
+    },
+    getNameGenre: function(){
+        return this.genre;
+    },
+    getNameCast: function(){
+         return this.cast[0].name;
+    },
+    setName: function(newTitle){
+        this.title = newTitle;
+    },
+    setName:function(newImage) {
+        this.Image = newImage;
+    }
+};
+
+console.log(`Judul Film: ${movie.getNameJudul()}, Gambar film: ${movie.getNameGambar()}, Genre: ${movie.getNameGenre()}`);
+
+console.log(`Nama Cast: ${movie.getNameCast()}`);
+
+this.newTitle = "Avengers age of ultron";
+console.log(`New Title: ${this.newTitle}`);
+this.newImage = "Full Robot";
+console.log(`New Image: ${this.newImage}`);
